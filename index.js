f73c9bc8a810d042285896dd8037ae92e4a2db98
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-04-18 15:21:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  var b = Object.assign({},object);
+  delete b[key];
+  return b
+}
